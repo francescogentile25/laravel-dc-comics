6355b9f4f053e12155da61f015b13b7ded59b3db
francescogentile25 <francescogentile250893@gmail.com>
2023-04-14 15:26:42 +0200

    FEAT  extend layout app.blade, created table with column

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function homepage()
+    {
+        $comics = Comic::all();
+        $data = [
+            'comics' => $comics
+        ];
+        return view('welcome', $data);
+    }
+}
